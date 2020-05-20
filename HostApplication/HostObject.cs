ad6e694a37e966c7037924321ecc9cabf7f0de70
Breahkd46 <34002356+Breahkd46@users.noreply.github.com>
2020-05-20 10:01:16 +0200

    tp1

---
+++ HostApplication/HostObject.cs
@@ -0,0 +1,19 @@
+﻿using RemotingObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostApplication
+{
+    class HostObject : MarshalByRefObject, RemotingObjects.IRemoteObjects
+    {
+        private int c = 0;
+        public string sayHello(string name)
+        {
+            c++;
+            return "Hello" + name + "number of invocation : " + c.ToString();
+        }
+    }
+}
---
+++ HostApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpChannel channel = new TcpChannel(12000);
+                ChannelServices.RegisterChannel(channel, true);
+                RemotingConfiguration.RegisterWellKnownServiceType(
+                    typeof(HostObject), "HostObject", WellKnownObjectMode.SingleCall);
+                Console.WriteLine("Remote object ready at server side!");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Error creating ");
+            }
+        }
+    }
+}
